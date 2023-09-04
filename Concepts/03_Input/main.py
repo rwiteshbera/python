da6e56748f9fafa4b28af015375b26e3d44f736f
rwiteshbera <rwiteshbera@gmail.com>
2023-09-05 01:15:54 +0530

    added input, string, list

--- Concepts/03_Input/main.py
+++ Concepts/03_Input/main.py
@@ -1,3 +1,25 @@
-value = input("Hello, What is your name? ") # Taking string input by default
-print(value)
-print(type(value))
+# value = input("Hello, What is your name? ") # Taking string input by default
+# print(value)
+# print(type(value))
+
+# Integer Input
+# age = int(input("Ente age: "))
+# print(age)
+# print(type(age))
+
+# Float
+# percentage = float(input("Enter percentage: "))
+# print(percentage)
+# print(type(percentage))
+
+# Multiple Input at a time
+# a, b, c = input("Enter three values: ").split()
+# print(f"type = {type(a)} | a = {a}")
+# print(f"type = {type(b)} | b = {b}")
+# print(f"type = {type(c)} | c = {c}")
+
+# Typecasting to int after taking multiple input a time
+a, b, c = list(map(int, input("Enter three values: ").split()))
+print(f"type = {type(a)} | a = {a}")
+print(f"type = {type(b)} | b = {b}")
+print(f"type = {type(c)} | c = {c}")
